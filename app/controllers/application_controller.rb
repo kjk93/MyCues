3206de561b39ad462c0f49bdce3b463d826d63de
Kyle Kravette <kkravette@gmail.com>
2016-02-14 01:03:28 -0500

    Added back menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  before_action :clear_object_name
+
   def logged_in_user
   	unless logged_in?
   		flash[:danger] = "Please Log In"
   		redirect_to login_url
   	end
+  end
+
+  def clear_object_name
+    session.delete(:obj)
   end
 end
