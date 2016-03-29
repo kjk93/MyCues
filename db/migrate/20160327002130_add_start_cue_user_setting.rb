9e7c83d058cc156a7e7fede8b5409c6939b07fc3
Kyle Kravette <kkravette@gmail.com>
2016-03-29 00:11:23 -0400

    Fixed migration issues

--- db/migrate/20160327002130_add_start_cue_user_setting.rb
+++ db/migrate/20160327002130_add_start_cue_user_setting.rb
@@ -1,6 +1,5 @@
 class AddStartCueUserSetting < ActiveRecord::Migration
   def change
   	add_column :user_settings, :start_cue, :integer, default: 1
-  	remove_column :users, :default_cue_time, :decimal
   end
 end
